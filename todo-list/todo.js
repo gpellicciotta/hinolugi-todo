dbabd248e404fd020cc45c6507ada26ed40651e2
Giovanni Pellicciotta <giovanni.pellicciotta@gmail.com>
2022-07-30 11:20:12 +0200

    Using localStorage for todo-list

--- todo-list/todo.js
+++ todo-list/todo.js
@@ -1,93 +1,112 @@
 document.addEventListener('DOMContentLoaded', onDomReady);
 
-const DEMO_ITEMS = [
-  {
-    "id": "item-1",
-    "status": "done",
-    "note": "Setup a new project",
-    "created-timestamp": "2022-07-07T14:14:00",
-    "last-modified-timestamp": "2022-07-07T14:16:00",
-  },
-  {
-    "id": "item-2",
-    "status": "active",
-    "note": "Make it work",
-    "created-timestamp": "2022-07-07T14:16:30",
-    "last-modified-timestamp": "2022-07-07T14:16:30",
-  },
-  {
-    "id": "item-4",
-    "status": "todo",
-    "note": "Add browser-based local persistance",
-    "created-timestamp": "2022-07-07T14:17:00",
-    "last-modified-timestamp": "2022-07-07T14:17:00",
-  },
-];
+const EMPTY_STATE = '{ "max-item-id": 99, "items": [] }';
 
-let maxItemId = 100;
+let maxItemId = 99;
+let items = [];
+let itemsById = new Map();
 
-function getTodoItemsFromStorage() {
-  // TODO: real implementation
-  return DEMO_ITEMS;
+function loadFromStorage() {
+  // Load state:
+  let stateJSON = localStorage.getItem('todo') || EMPTY_STATE;
+  console.debug("Loaded from storage: " + stateJSON);
+  let state = JSON.parse(stateJSON);  
+  maxItemId = state["max-item-id"] || 100;
+  // Fill state:
+  items = state["items"] || [];
+  itemsById.clear();
+  items.forEach((item) => { itemsById.set(item.id, item); });
+}
+
+function saveToStorage() {
+  let state = {
+    "max-item-id": maxItemId,
+    "items": items
+  };  
+  let stateJSON = JSON.stringify(state, ' ');
+  try {
+    localStorage.setItem('todo', stateJSON);
+    console.debug("Saving to storage succeeded");
+  }
+  catch (error) {
+    // TODO: show notification
+    console.error('Failed to store state: ', error);
+  }
 }
 
 function onDomReady() {
+  // Get new-item input + register 'enter' action
+  let newItemInput = document.getElementById("new-item");
+  newItemInput.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+      addNewItem(event);
+    }
+  });
   // Get add-item button + register 'click' action
   let addItemButton = document.getElementById("add-new-item");
   addItemButton.addEventListener('click', addNewItem);
 
-  // Add items from storage
+  // Get todo-list + register 'click' action
   let todoList = document.getElementById("todo-list");
-  todoList.innerHTML = ''; // Empty
+  todoList.addEventListener('click', onListClick);
 
-  let todoItems = getTodoItemsFromStorage();
-  todoItems.forEach(addItem);
+  // Add items from storage
+  loadFromStorage();
+
+  // Re-build from storage
+  createItemList();
 
   // Ensure input has focus
-  let newItemInput = document.getElementById("new-item");
   newItemInput.focus();
 }
 
-function addItem(item) {
+function onListClick(event) {
+  let targetElement = event.target;
+  let actionElement = targetElement.closest("[data-action]");
+  let liElement = targetElement.closest("li.item");
+  console.debug("Click on target: " + targetElement.outerHTML + " within list element " + liElement.outerHTML);
+  if (actionElement && (actionElement.dataset.action === 'delete')) {
+    event.stopPropagation();
+    deleteItem(+liElement.dataset.itemId);
+  }
+  else if (actionElement && (actionElement.dataset.action === 'toggle')) {
+    event.stopPropagation();
+    markItem(+liElement.dataset.itemId);
+  }
+}
+
+function createItemList() {
+  let todoList = document.getElementById("todo-list");
+  todoList.innerHTML = ''; // Empty
+  items.forEach(addItemToList);
+}
+
+function addItemToList(item) {
   let todoList = document.getElementById("todo-list");
 
   let newLiItem = document.createElement("li");
-  newLiItem.setAttribute("id", item["id"]);
+  newLiItem.setAttribute("id", "item-" + item.id);
+  newLiItem.dataset.itemId = item.id;
   newLiItem.classList.add("item");
   newLiItem.innerHTML = `
-    <label id="${item["id"]}-checkbox-label"><i class="fa-solid fa-check"></i><input type="checkbox" id="${item["id"]}-checkbox"></label>
+    <label data-action="toggle" data-item-id="${item.id}"><i class="fa-solid fa-check"></i></label>
     <div class="item-content">
-      <span class="note">${item["note"]}</span>
+      <span data-action="edit" data-item-id="${item.id}" class="note">${item["note"]}</span>
       <div class="tags">
-        <span class="status tag">done</span>
+        <span class="status tag" data-action="toggle" data-item-id="${item.id}">done</span>
         <span class="created tag">${item["created-timestamp"]}</span>
         <span class="last-modified tag">${item["last-modified-timestamp"]}</span>
       </div>
     </div>
-    <button class="icon-button" id="${item["id"]}-delete" title="Delete"><i class="fa-solid fa-trash"></i></button>
+    <button class="icon-button" data-action="delete" data-item-id="${item.id}" title="Delete"><i class="fa-solid fa-trash"></i></button>
   `;
   todoList.appendChild(newLiItem);
-
-  let deleteButton = document.getElementById(item["id"] + "-delete");
-  deleteButton.addEventListener("click", () => {
-    newLiItem.remove();
-  });
-
-  let checkbox = document.getElementById(item["id"] + "-checkbox");
-  checkbox.addEventListener("click", () => {
-    if (checkbox.checked) {
-      newLiItem.classList.add("done");
-    }
-    else {
-      newLiItem.classList.remove("done");
-    }
-  });
 }
 
 function addNewItem(event) {
   let newItemInput = document.getElementById("new-item");
   let itemText = newItemInput.value;
-  let itemId = "item-" + (maxItemId++);
+  let itemId = (++maxItemId);
   let creationDate = new Date();
   let newItem = {
     "id": itemId,
@@ -96,15 +115,67 @@ function addNewItem(event) {
     "created-timestamp": creationDate.toISOString(),
     "last-modified-timestamp": creationDate.toISOString(),
   };
-  addItem(newItem);
+  items.push(newItem);
+  itemsById.set(newItem.id, newItem);
+  saveToStorage();
+
+  addItemToList(newItem, items.length - 1);  
   newItemInput.value = '';
   newItemInput.focus();
 }
 
-function markItem(event) {
-
+function indexOfItem(id) {  
+  for (let i = 0; i < items.length; i++) {  
+    if (items[i].id === id) {
+      return i;
+    }
+  }
+  return -1;
 }
 
-function deleteItem(event) {
+function markItem(id) {
+  let item = itemsById.get(id)
+  if (!item) {
+    console.error(`Cannot mark item with invalid id ${id}`);
+    return ;
+  }
+  // Update model:
+  if (item.status === 'done') {
+    item.status = 'todo';
+  }
+  else {
+    item.status = 'done';
+  }
+  saveToStorage();
+  // Update UI:
+  let itemEl = document.getElementById("item-" + id);
+  if (!itemEl) {
+    console.error(`Cannot find item element with id ${id}`);
+    return;
+  }
+  if (item.status === 'done') {
+    itemEl.classList.add("done");
+  }
+  else {
+    itemEl.classList.remove("done");
+  }
+}
 
+function deleteItem(id) {
+  let idx = indexOfItem(id);
+  if (idx < 0) {
+    console.error(`Cannot delete item with invalid id ${id}`);
+    return;
+  }
+  // Update model:
+  items.splice(idx, 1);
+  itemsById.delete(id);
+  saveToStorage();
+  // Update UI:
+  let itemEl = document.getElementById("item-" + id);
+  if (!itemEl) {
+    console.error(`Cannot find item element with id ${id}`);
+    return;
+  }
+  itemEl.remove();
 }
