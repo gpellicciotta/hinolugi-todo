646379174a9b2c06556d4559e0bfaf00642015ca
Giovanni Pellicciotta <giovanni.pellicciotta@gmail.com>
2022-08-11 15:08:30 +0200

    Getting app name, version and description from actual manifest Small layout improvements

--- utils.js
+++ utils.js
@@ -1,11 +1,42 @@
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function () {
+window.addEventListener('load', onFullyLoaded);
+
+function onFullyLoaded() {
+  registerServiceWorker();
+  fixVersion();
+}
+
+function registerServiceWorker() {
+  if ("serviceWorker" in navigator) {
     navigator.serviceWorker
       .register("/service-worker.js")
-      .then(res => console.info("utils:service-worker: registered now", res))
-      .catch(err => console.error("utils:service-worker: failed to register", err))
-  })
+      .then(res => console.info("service-worker has been registered", res))
+      .catch(err => console.error("service-worker failed to register", err));
+  }
+  else {
+    console.warn("current browser doesn't support service workers - the web-app cannot be installed locally");
+  }
 }
-else {
-  console.warn("Current browser doesn't support service workers - the web-app cannot be installed locally");
+
+function fixVersion() {
+  fetch('/manifest.json')
+    .then(response => response.json())
+    .then(manifest => {
+      console.info('manifest', manifest);
+      const appVersion = manifest.version || 'unknown';
+      console.info(`application version: ${appVersion}`);
+      const appName = manifest.name || 'HiNoLuGi Utils';
+      console.info(`application name: ${appName}`);
+      const appDescription = manifest.description || 'A collection of small utilities and tools.';
+      console.info(`application description: ${appDescription}`);
+      document.querySelectorAll(".app-version").forEach(el => {
+        el.innerText = appVersion;
+      });
+      document.querySelectorAll(".app-name").forEach(el => {
+        el.innerText = appName;
+      });
+      document.querySelectorAll(".app-description").forEach(el => {
+        el.innerText = appDescription;
+      });
+    })
+    .catch(err => console.error("cannot load manifest.json:", err));
 }
